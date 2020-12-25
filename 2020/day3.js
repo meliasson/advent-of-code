3897269f8e087f05a40abb21196fd6bd13bb97cb
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2020-12-25 10:07:17 +0100

    2020: Day 3

---
+++ 2020/day3.js
@@ -0,0 +1,50 @@
+const { getInput } = require('./input')
+
+const part1 = () => {
+  const map = getMap()
+  const slopes = [{ x: 3, y: 1 }]
+  return countTreesInSlopes(map, slopes)
+}
+
+const part2 = () => {
+  const map = getMap()
+  const slopes = [
+    { x: 1, y: 1 },
+    { x: 3, y: 1 },
+    { x: 5, y: 1 },
+    { x: 7, y: 1 },
+    { x: 1, y: 2 },
+  ]
+  return countTreesInSlopes(map, slopes)
+}
+
+const getMap = () => {
+  return getInput('day3-input')
+}
+
+const countTreesInSlopes = (map, slopes) => {
+  const results = []
+  for (var i = 0; i < slopes.length; i++) {
+    const result = countTreesInSlope(map, slopes[i])
+    results.push(result)
+  }
+  return results.reduce((acc, cur) => acc * cur)
+}
+
+const countTreesInSlope = (map, slope) => {
+  let result = 0
+  for (
+    let rowIndex = 0, colIndex = 0;
+    rowIndex < map.length;
+    rowIndex = rowIndex + slope.y, colIndex = colIndex + slope.x
+  ) {
+    const pos = map[rowIndex][colIndex % map[rowIndex].length]
+    if (pos === '#') result++
+  }
+  return result
+}
+
+module.exports = {
+  part1,
+  part2,
+}
---
+++ 2020/test/day3.spec.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const { part1, part2 } = require('../day3')
+
+describe('Day 3', function () {
+  describe('part 1', function () {
+    it('counts number of trees encountered', function () {
+      const result = part1()
+      assert.equal(result, 156)
+    })
+  })
+
+  describe('part 2', function () {
+    it('counts number of trees encountered on multiple slopes', function () {
+      const result = part2()
+      assert.equal(result, 3521829480)
+    })
+  })
+})
