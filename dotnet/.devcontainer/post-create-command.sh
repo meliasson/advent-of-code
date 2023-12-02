be51d9a1c5479bbc3eb90781ac1480e51ba35bee
Mikael Eliasson <mikael.e.eliasson@gmail.com>
2023-12-02 21:20:56 +0000

    Skip profile in PowerShell extension terminal
    It complains about posh-git plus I like to be able to easily tell it
    apart from the pwsh terminal.

--- dotnet/.devcontainer/post-create-command.sh
+++ dotnet/.devcontainer/post-create-command.sh
@@ -4,8 +4,6 @@
 mkdir -p /home/vscode/.config/powershell
 cp ./.devcontainer/PowerShellProfile.ps1 \
     /home/vscode/.config/powershell/Microsoft.PowerShell_profile.ps1
-ln -s /home/vscode/.config/powershell/Microsoft.PowerShell_profile.ps1 \
-    /home/vscode/.config/powershell/Microsoft.VSCode_profile.ps1
 
 # Avoid get trapped in GNU nano or similar.
 if [ "$(git config core.editor)" != 'code --wait' ]; then
