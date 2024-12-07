8f59b75a6b11bbceb32e11d6c3194ebb37f3312d
Mikael Eliasson <mikael.eliasson@biometria.se>
2024-12-07 19:02:32 +0000

    Day 3 part 2 2024

--- dotnet/powershell/2024/Day3Year2024.Tests.ps1
+++ dotnet/powershell/2024/Day3Year2024.Tests.ps1
@@ -4,18 +4,28 @@ BeforeAll {
     Import-Module -Force "$PSScriptRoot/Day3Year2024.psm1"
 
     function Get-PuzzleInput {
-        'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
+
     }
 }
 
 Describe 'Day 3' {
     It 'part 1' {
         Mock -ModuleName 'Day3Year2024' -CommandName Get-Content -MockWith {
-            Get-PuzzleInput
+            'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
         }
 
         $result = Get-Day3Part1Year2024
 
         $result | Should -Be 161
+    }
+
+    It 'part 2' {
+        Mock -ModuleName 'Day3Year2024' -CommandName Get-Content -MockWith {
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        }
+
+        $result = Get-Day3Part2Year2024
+
+        $result | Should -Be 48
     }
 }
--- dotnet/powershell/2024/Day3Year2024.psm1
+++ dotnet/powershell/2024/Day3Year2024.psm1
@@ -5,8 +5,27 @@ function Get-Day3Part1Year2024 {
         Select-String -Pattern 'mul\((\d*),(\d*)\)' -AllMatches |
         Select-Object -ExpandProperty 'Matches' |
         ForEach-Object {
-            Write-Host ($_ | Out-String)
             [int]$_.Groups[1].Value * [int]$_.Groups[2].Value
+        } |
+        Measure-Object -Sum |
+        Select-Object -ExpandProperty 'Sum'
+}
+
+function Get-Day3Part2Year2024 {
+    $enabled = $true
+    Get-Content -Path "$PSScriptRoot/Day3Year2024.input" -Raw |
+        Select-String -Pattern "mul\((\d*),(\d*)\)|(do\(\))|(don\'t\(\))" -AllMatches |
+        Select-Object -ExpandProperty 'Matches' |
+        ForEach-Object {
+            if ($_.Groups[0].Value -eq 'do()') {
+                $enabled = $true
+                0
+            } elseif ($_.Groups[0].Value -eq "don't()") {
+                $enabled = $false
+                0
+            } elseif ($enabled) {
+                [int]$_.Groups[1].Value * [int]$_.Groups[2].Value
+            }
         } |
         Measure-Object -Sum |
         Select-Object -ExpandProperty 'Sum'
@@ -14,7 +33,8 @@ function Get-Day3Part1Year2024 {
 
 $exportModuleMemberArgs = @{
     Function = (
-        'Get-Day3Part1Year2024'
+        'Get-Day3Part1Year2024',
+        'Get-Day3Part2Year2024'
     )
 }
 Export-ModuleMember @exportModuleMemberArgs
